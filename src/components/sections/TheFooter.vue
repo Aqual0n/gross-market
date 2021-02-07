9ca2623e7eafbe5991a37f351eb008c8627e8cd9
Slava <vl@burno.tech>
2021-02-07 21:06:05 +0400

    feat(sections): footer added

---
+++ src/components/sections/TheFooter.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+include ../../../tools/mixins.pug
++b.FOOTER.footer
+    +e.container--top.container
+        +e.panel
+            +e.logo
+                img(
+                    src="/images/header/logo.svg"
+                )
+            +e.share поделиться
+            +e.socials
+                +e.A.social--fb(
+                    href="#"
+                    target="_blank"
+                )
+                +e.A.social--vk(
+                    href="#"
+                    target="_blank"
+                )
+    +e.container.container
+        +e.bottom
+            +e.privacy(
+                v-on:click="$emit('openPopup')"
+            ) Политика обработки персональных данных
+            +e.copyright © Гросс маркет 2020
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -5,6 +5,7 @@ div
     vacancies-component
     instagram-component
     geography-component
+    footer-component
 </template>
 
 <script>
@@ -13,6 +14,7 @@ import Main from '../components/sections/Main.vue';
 import Vacancies from '../components/sections/Vacancies.vue';
 import Instagram from '../components/sections/Instagram.vue';
 import Geography from '../components/sections/Geography.vue';
+import TheFooter from '../components/sections/TheFooter.vue';
 export default {
     components: {
         'header-component': TheHeader,
@@ -20,6 +22,7 @@ export default {
         'vacancies-component': Vacancies,
         'instagram-component': Instagram,
         'geography-component': Geography,
+        'footer-component': TheFooter,
     },
 };
 </script>
